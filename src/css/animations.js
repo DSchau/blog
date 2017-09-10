70f7471ccd02b9133a56129abaf33d7f27706405
Dustin Schau <dustinschau@gmail.com>
2017-09-10 11:17:16 -0500

    style: use keyframes animation for styled-components; tweak build

--- src/css/animations.js
+++ src/css/animations.js
@@ -1,5 +1,6 @@
-export const animateBackground = `
-  @keyframes animateBg { 
+import { keyframes } from 'styled-components';
+
+export const animateBackground = keyframes`
     0%{
       background-position: 50% 0%;
     }
@@ -9,37 +10,32 @@ export const animateBackground = `
     100%{
       background-position:50% 0%;
     }
+`;
+
+/*
+* https://css-tricks.com/snippets/css/shake-css-keyframe-animation/
+*/
+export const animateShake = keyframes`
+  10%, 90% {
+    transform: translate3d(-1px, 0, 0) rotate(5deg);
   }
-`.trim()
-
-export const animateShake = `
-  /*
-  * https://css-tricks.com/snippets/css/shake-css-keyframe-animation/
-  */
-  @keyframes shake {
-    10%, 90% {
-      transform: translate3d(-1px, 0, 0) rotate(5deg);
-    }
-    
-    20%, 80% {
-      transform: translate3d(2px, 0, 0);
-    }
-
-    30%, 50%, 70% {
-      transform: translate3d(-2px, 0, 0) rotate(-5deg);
-    }
-
-    40%, 60% {
-      transform: translate3d(2px, 0, 0);
-    }
+  
+  20%, 80% {
+    transform: translate3d(2px, 0, 0);
   }
-`.trim()
 
-export const fadeInBottom = `
-  @keyframes fade-in-bottom {
-    to {
-      transform: translateY(0);
-      opacity: 1;
-    }
+  30%, 50%, 70% {
+    transform: translate3d(-2px, 0, 0) rotate(-5deg);
+  }
+
+  40%, 60% {
+    transform: translate3d(2px, 0, 0);
+  }
+`
+
+export const fadeInBottom = keyframes`
+  to {
+    transform: translateY(0);
+    opacity: 1;
   }
 `
--- src/pages/404.js
+++ src/pages/404.js
@@ -9,12 +9,11 @@ import ImageShift from '../components/ImageShift'
 import FourOhFour from '../images/404.jpeg'
 
 const Container = styled.div`
-  ${fadeInBottom};
   max-width: 100%;
   transform: translateY(16px) scale(.99);
   transform-origin: 50% 0;
   opacity: 0;
-  animation: fade-in-bottom 0.3s cubic-bezier(.39, .575, .565, 1) both;
+  animation: ${fadeInBottom} 0.3s cubic-bezier(.39, .575, .565, 1) both;
 `
 
 const ImageContainer = styled.div`
