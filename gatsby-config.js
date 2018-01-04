57d85936b479013355ca1db67d0eab76130f8eef
Dustin Schau <dustinschau@gmail.com>
2018-01-03 20:54:48 -0600

    chore: move blog posts to posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,8 +11,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
+        path: `${__dirname}/src/posts`,
+        name: 'posts',
       },
     },
     {
