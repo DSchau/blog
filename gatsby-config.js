99b62b26fe38160fbdaffc436b21ea52e3766c6a
Dustin Schau <DSchau@users.noreply.github.com>
2018-02-09 17:25:14 +0100

    Update deps (#44)
    * chore: update deps, tweak content path
    
    * chore: add renovate config
    
    * chore: fix build

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/posts`,
+        path: `${__dirname}/content/blog`,
         name: 'posts',
       },
     },
