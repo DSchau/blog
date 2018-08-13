eed98a3afa5bbcebe1d88d9a5e15a5382359a42b
Dustin Schau <DSchau@users.noreply.github.com>
2018-08-13 17:45:40 -0400

    fix: add blur up effect (#84)
    _Ideally_ this would have an intersection observer, but future problems 🙌

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,7 @@ module.exports = {
           {
             resolve: 'gatsby-remark-images',
             options: {
+              backgroundColor: 'transparent',
               linkImagesToOriginal: false
             }
           },
