3e0d2cede1eb42b761fdf1537e8b90af8795c601
Dustin Schau <DSchau@users.noreply.github.com>
2018-12-28 16:04:54 -0600

    feat: migrate to the new blog theme (#100)
    This PR does a few things, specifically:
    
    - Migrates to [@dschau/gatsby-theme-blog](https://github.com/DSchau/gatsby-blog-theme)
    - Upgrades to emotion 10
    - Tweaks filesystem a bit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,69 +1,10 @@
 module.exports = {
-  pathPrefix: '/',
-  siteMetadata: {
-    title: 'Dustin Schau - Blog',
-    author: 'Dustin Schau',
-  },
-  plugins: [
+  __experimentalThemes: [
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: '@dschau/gatsby-theme-blog',
       options: {
-        name: `Dustin Schau's Blog`,
-        description: 'The blog of the developer, Dustin Schau',
-        short_name: 'DSchau Blog',
-        background_color: 'white',
-        theme_color: '#002635',
-        orientation: 'portrait',
-        display: 'minimal-ui'
-      }
-    }, 
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-emotion',
-    'gatsby-plugin-remove-trailing-slashes',
-    'gatsby-plugin-twitter',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: 'post',
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          'gatsby-remark-copy-linked-files',
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              backgroundColor: 'transparent',
-              linkImagesToOriginal: false,
-              showCaptions: true
-            }
-          },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-smartypants',
-          'gatsby-remark-autolink-headers'
-        ]
-      }
-    },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-layout',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-offline',
-    {
-      resolve: 'gatsby-plugin-typography',
-      options: {
-        omitGoogleFont: true,
-        pathToConfigModule: 'src/utils/typography'
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: 'UA-102928446-2'
+        root: __dirname
       }
     }
-  ],
+  ]
 }
--- src/css/queries.js
+++
@@ -1,3 +0,0 @@
-export const small = `320px`
-export const medium = `480px`
-export const large = `768px`
