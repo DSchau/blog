204b988ceb71bb5f27b66c3afba6a8562873c99e
Dustin Schau <dustinschau@gmail.com>
2019-01-04 11:30:35 -0600

    chore: add files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,24 @@
 process.noDeprecation = true; // https://github.com/webpack/webpack/issues/6568
 
+const siteUrl =
+  process.env.URL || process.env.DEPLOY_URL || "https://blog.dustinschau.com";
+
 module.exports = {
+  siteMetadata: {
+    author: "Dustin Schau",
+    description:
+      "The blog of the Omaha, Nebraska based software engineer, Dustin Schau",
+    siteUrl,
+    social: {
+      twitter: "schaudustin"
+    }
+  },
   __experimentalThemes: [
     {
-      resolve: '@dschau/gatsby-theme-blog',
+      resolve: "@dschau/gatsby-theme-blog",
       options: {
         root: __dirname
       }
     }
   ]
-}
+};
